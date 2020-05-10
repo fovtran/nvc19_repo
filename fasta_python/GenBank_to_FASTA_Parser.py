8fad14ffbe4f08f9c5dc972633e250f0813b8f7d
Diego <recsvintage@gmail.com>
2020-05-10 17:18:51 -0300

    Biopython support files

--- fasta_python/GenBank_to_FASTA_Parser.py
+++ fasta_python/GenBank_to_FASTA_Parser.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-"""
-
 from Bio import SeqIO
 import os
 
@@ -21,6 +16,6 @@ if not os.path.exists(output_file_name): #checks for a pre-existing file with th
                 if any("protein" in s for s in val): #Finds all the CDS which contain the word "protein" in the qualifiers. Change to 'if "Name" in val:' for protein called "name" exactly
                     with open(output_file_name, "a") as ofile: #opens the output file and "a" designates it for appending
                         ofile.write(">{0}| {1}| {2}| {3}| {4}| \n{5}\n\n".format(acc, organism, tax_line, feature.qualifiers['protein_id'][0], val[0], feature.qualifiers['translation'][0])) #Writes my FASTA format sequences to the output file
-                        
+
 else:
     print ("The output file already seem to exist in the current working directory {0}. Please change the name of the output file".format(os.getcwd())) #error code, so you don't overwirite your files
---
+++ python-covid/read_pdb_biopython.py
@@ -0,0 +1,30 @@
+from Bio.PDB import PDBParser, Selection
+from Bio.PDB.PDBIO import PDBIO
+import nglview as nv
+
+parser = PDBParser()
+structure = parser.get_structure("1a8o", "structures_4_3_2020/m_protein.pdb")
+
+print(structure)
+view = nv.show_biopython(structure)
+view
+
+for atom in structure.get_atoms():
+    print ( atom )
+
+for model in structure:
+    for chain in model:
+        for residue in chain:
+            for atom in residue:
+                print ( atom )
+
+res_list = Selection.unfold_entities(structure, 'R')
+# Get all atoms from a chain
+atom_list = Selection.unfold_entities(chain, 'A')
+print(res_list)
+print(atom_list)
+io=PDBIO()
+io.set_structure(structure)
+io.save("bio-pdb-pdbio-out.pdb")
+import os
+os.remove("bio-pdb-pdbio-out.pdb")  # tidy up
