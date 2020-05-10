f07cd37c0fe4d0d8ca53a5b3fa42dcde9fc87dc3
Diego <recsvintage@gmail.com>
2020-05-10 17:27:40 -0300

    PDBx updates

--- python-covid/download-pdb-structures.py
+++ python-covid/download-pdb-structures.py
@@ -4,6 +4,8 @@ from Bio.PDB import PDBList
 
 '''Selecting structures from PDB'''
 pdbl = PDBList()
-PDBlist2=['4B97','4IPH','4HNO','4HG7','4IRG','4G4W','4JKW','4IPC','2YPM','4KEI']
-for i in PDBlist2:
+PDBtestlist=['4B97','4IPH','4HNO','4HG7','4IRG','4G4W','4JKW','4IPC','2YPM','4KEI']
+PDBlist2_ncov19=['6LU7', '6VSB', '6LXT', '6LVN', '6VW1', '6VWW', '6Y2E', '6Y2F', '6Y2G']
+
+for i in PDBlist2_ncov19:
     pdbl.retrieve_pdb_file(i,pdir='PDB')
